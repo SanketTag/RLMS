92484e061dee873711183211a9527deb110ece52
AbhishekTag <tagalpallewar.abhishek@gmail.com>
2017-02-18 10:10:56 +0530

    changed controller

--- src/main/webapp/scripts/app/controllers/addCompanyCtrl.js
+++ src/main/webapp/scripts/app/controllers/addCompanyCtrl.js
@@ -52,6 +52,55 @@
 		$scope.backPage =function(){
 			 $window.history.back();
 		}
+		//add user ctrl
+		initAddUser();
+		loadCompayInfo();
+		function initAddUser(){
+			$scope.selectedCompany = {};
+			$scope.addUser={
+				firstName:'',
+				lastName:'',
+				address:'',
+				contactNumber:'',
+				emailId:'',
+				companyId:''	
+			};	
+		    //$scope.companies = [];
+		    $scope.userList={};
+		}
+		function loadCompayInfo(){
+			serviceApi.doPostWithoutData('/RLMS/admin/getAllApplicableCompanies')
+		    .then(function(response){
+		    		$scope.companies = response;
+		    });
+		};
+		$scope.submitAddUser = function(){
+			$scope.addUser.companyId = $scope.selectedCompany.selected.companyId;
+			serviceApi.doPostWithData("/RLMS/admin/validateAndRegisterNewUser",$scope.addUser)
+			.then(function(response){
+				$scope.showAlert = true;
+				var key = Object.keys(response);
+				var successMessage = response[key[0]];
+				$scope.alert.msg = successMessage;
+				$scope.alert.type = "success";
+				//utility.showMessage("",successMessage,"success");
+				initAddUser();
+				$scope.addUserForm.$setPristine();
+				$scope.addUserForm.$setUntouched();
+				
+			},function(error){
+				$scope.showAlert = true;
+				$scope.alert.msg = error.exceptionMessage;
+				$scope.alert.type = "danger";
+			});
+		}
+		//rese add branch
+		$scope.resetAddUser = function(){
+			$scope.showAlert = false;
+			initAddUser();
+			$scope.addUserForm.$setPristine();
+			$scope.addUserForm.$setUntouched();
+		}
 		
 	}]);
 })();
