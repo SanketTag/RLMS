5eab5a907127afb28d23e07c8af71f59572884b7
AbhishekTag <tagalpallewar.abhishek@gmail.com>
2017-05-25 13:39:17 +0530

    inprogress configure lift
    made next and back button working

--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -389,6 +389,7 @@
 	<script src="scripts/app/controllers/reportsController.js"></script>
 	<script src="scripts/app/controllers/addAmcCtrl.js"></script>
 	<script src="scripts/app/controllers/technicianReportCtrl.js"></script>
+	<script src="scripts/app/controllers/configureLiftController.js"></script>
 	<script src="scripts/app/services/serviceApi.js"></script>
 	<script src="scripts/app/services/utility.js"></script>
 	<!-- App-->
--- src/main/webapp/scripts/app/controllers/configureLiftController.js
+++ src/main/webapp/scripts/app/controllers/configureLiftController.js
@@ -1,19 +1,30 @@
 (function () {
     'use strict';
 	angular.module('rlmsApp')
-	.controller('reportCtrl', ['$scope', '$filter','serviceApi','$route','$http','utility','$rootScope', function($scope, $filter,serviceApi,$route,$http,utility,$rootScope) {
+	.controller('configurLiftController', ['$scope', '$filter','serviceApi','$route','$http','utility','$rootScope', function($scope, $filter,serviceApi,$route,$http,utility,$rootScope) {
 		initConfigLift();
+		$scope.changeTab = function(targetIndex){
+			angular.element('#tab'+targetIndex).trigger('click');
+		}
 		function initConfigLift(){
 			$scope.a={};
 			$scope.bd={};
 			$scope.p={};
 			$scope.rtc={};
-			$scope.a.homeLandingFloorNumber = 0;
+			$scope.a.homeLandingFloorNumber = false;
 			$scope.a.disableHomelanding = false;
 			$scope.a.homeLandingTime = '';
 			$scope.a.fireModeFloorSelection = "";
-			Output contactor off delay time at lift stopping
-			
+			$scope.$watch(
+                    "a.disableHomelanding",
+                    function handleChange( newValue, oldValue ) {
+                    	if(newValue == true){
+                    		$scope.a.homeLandingFloorNumber = 99;
+                    	}else{
+                    		$scope.a.homeLandingFloorNumber = 0;
+                    	}
+                    }
+                );
 		}
 		
 	}]);
